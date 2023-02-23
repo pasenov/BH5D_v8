4ae3b3705b16847ecdaa70dd35a8aea0e8631d1a
pasenov <31407122+pasenov@users.noreply.github.com>
2023-02-23 17:53:22 +0100

    Add files via upload

---
+++ src/PhysicsList.cc
@@ -0,0 +1,256 @@
+//
+// ********************************************************************
+// * License and Disclaimer                                           *
+// *                                                                  *
+// * The  Geant4 software  is  copyright of the Copyright Holders  of *
+// * the Geant4 Collaboration.  It is provided  under  the terms  and *
+// * conditions of the Geant4 Software License,  included in the file *
+// * LICENSE and available at  http://cern.ch/geant4/license .  These *
+// * include a list of copyright holders.                             *
+// *                                                                  *
+// * Neither the authors of this software system, nor their employing *
+// * institutes,nor the agencies providing financial support for this *
+// * work  make  any representation or  warranty, express or implied, *
+// * regarding  this  software system or assume any liability for its *
+// * use.  Please see the license in the file  LICENSE  and URL above *
+// * for the full disclaimer and the limitation of liability.         *
+// *                                                                  *
+// * This  code  implementation is the result of  the  scientific and *
+// * technical work of the GEANT4 collaboration.                      *
+// * By using,  copying,  modifying or  distributing the software (or *
+// * any work based  on the software)  you  agree  to acknowledge its *
+// * use  in  resulting  scientific  publications,  and indicate your *
+// * acceptance of all terms of the Geant4 Software license.          *
+// ********************************************************************
+//
+/// \file electromagnetic/BH5D/src/PhysicsList.cc
+/// \brief Implementation of the PhysicsList class
+//
+//
+//
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+#include "PhysicsList.hh"
+#include "PhysicsListMessenger.hh"
+
+#include "G4EmStandardPhysics.hh"
+#include "PhysListEmStandard.hh"
+#include "G4DecayPhysics.hh"
+#include "G4EmStandardPhysics_option1.hh"
+#include "G4EmStandardPhysics_option2.hh"
+#include "G4EmStandardPhysics_option3.hh"
+#include "G4EmStandardPhysics_option4.hh"
+#include "G4EmStandardPhysicsSS.hh"
+#include "G4EmStandardPhysicsGS.hh"
+#include "G4EmLivermorePhysics.hh"
+#include "G4EmPenelopePhysics.hh"
+#include "G4HadronElasticPhysics.hh"
+#include "G4HadronElasticPhysicsXS.hh"
+#include "G4HadronElasticPhysicsHP.hh"
+#include "G4HadronHElasticPhysics.hh"
+#include "G4ChargeExchangePhysics.hh"
+#include "G4NeutronTrackingCut.hh"
+#include "G4NeutronCrossSectionXS.hh"
+#include "G4StoppingPhysics.hh"
+#include "G4IonBinaryCascadePhysics.hh"
+#include "G4IonPhysics.hh"
+#include "G4IonPhysicsXS.hh"
+#include "G4IonElasticPhysics.hh"
+#include "G4EmExtraPhysics.hh"
+#include "G4EmParameters.hh"
+#include "G4PhysListFactoryMessenger.hh"
+#include "MuNuclearBuilder.hh"
+
+#include "G4EmParameters.hh"
+
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
+
+#include "G4BosonConstructor.hh"
+#include "G4LeptonConstructor.hh"
+#include "G4MesonConstructor.hh"
+#include "G4BosonConstructor.hh"
+#include "G4BaryonConstructor.hh"
+#include "G4IonConstructor.hh"
+#include "G4ShortLivedConstructor.hh"
+
+#include "G4SystemOfUnits.hh"
+
+#include "FTFP_BERT.hh"
+#include "G4HadronPhysicsFTFP_BERT.hh"
+#include "G4HadronPhysicsFTFP_BERT_HP.hh"
+#include "G4HadronPhysicsFTFP_BERT_TRV.hh"
+#include "G4HadronPhysicsFTF_BIC.hh"
+#include "G4HadronInelasticQBBC.hh"
+#include "G4HadronPhysicsQGSP_BERT.hh"
+#include "G4HadronPhysicsQGSP_BERT_HP.hh"
+#include "G4HadronPhysicsQGSP_BIC.hh"
+#include "G4HadronPhysicsQGSP_BIC_HP.hh"
+#include "G4HadronPhysicsQGSP_FTFP_BERT.hh"
+#include "G4HadronPhysicsQGS_BIC.hh"
+#include "G4RadioactiveDecayPhysics.hh"
+
+#include "G4ProcessManager.hh"
+#include "G4ParticleTypes.hh"
+#include "G4ParticleTable.hh"
+#include "G4Gamma.hh"
+#include "G4Electron.hh"
+#include "G4Positron.hh"
+#include "G4Proton.hh"
+
+#include "G4RunManager.hh"
+#include "G4UImanager.hh"
+#include "G4SteppingVerbose.hh"
+#include "Randomize.hh"
+
+#include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
+#include "PrimaryGeneratorAction.hh"
+
+#include "G4UIExecutive.hh"
+#include "G4VisExecutive.hh"
+
+#include "G4Types.hh"
+
+#include "G4RunManagerFactory.hh"
+
+#include "G4GenericBiasingPhysics.hh"
+
+#include "G4PhysListFactory.hh"
+
+
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PhysicsList::PhysicsList() : G4VModularPhysicsList(),
+  fEmPhysicsList(0),
+  fMuNuclPhysicsList(0),
+  fMessenger(0)
+{
+  SetVerboseLevel(1);
+  fMessenger = new PhysicsListMessenger(this);
+
+  // EM physics
+  fEmName = G4String("emstandard_opt0");
+  fEmPhysicsList = new G4EmStandardPhysics();
+
+  //extend energy range of PhysicsTables
+  //
+  G4EmParameters* param = G4EmParameters::Instance();
+  param->SetMinEnergy(100*eV);  
+  param->SetMaxEnergy(1000*PeV);
+
+  fMuNuclPhysicsList = 0;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PhysicsList::~PhysicsList()
+{
+  delete fMessenger;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PhysicsList::ConstructParticle()
+{
+  G4BosonConstructor  pBosonConstructor;
+  pBosonConstructor.ConstructParticle();
+
+  G4LeptonConstructor pLeptonConstructor;
+  pLeptonConstructor.ConstructParticle();
+
+  G4MesonConstructor pMesonConstructor;
+  pMesonConstructor.ConstructParticle();
+
+  G4BaryonConstructor pBaryonConstructor;
+  pBaryonConstructor.ConstructParticle();
+
+  G4IonConstructor pIonConstructor;
+  pIonConstructor.ConstructParticle();
+
+  G4ShortLivedConstructor pShortLivedConstructor;
+  pShortLivedConstructor.ConstructParticle();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PhysicsList::ConstructProcess()
+{
+  // transportation
+  //
+  AddTransportation();
+  
+  // electromagnetic Physics List
+  //
+  fEmPhysicsList->ConstructProcess();
+  if(fMuNuclPhysicsList) fMuNuclPhysicsList->ConstructProcess();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PhysicsList::AddPhysicsList(const G4String& name)
+{
+  if (verboseLevel>-1) {
+    G4cout << "PhysicsList::AddPhysicsList: <" << name << ">" << G4endl;
+  }
+
+  if (name == fEmName) return;
+
+  /*if (name == "emstandard_opt0") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysics();
+
+  } else if (name == "emstandard_opt4") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    physicsList = new G4EmStandardPhysics_option4();
+ 
+  } else if (name == "emstandardSS") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new G4EmStandardPhysicsSS();
+
+  } else if (name == "local") {
+
+    fEmName = name;
+    delete fEmPhysicsList;
+    fEmPhysicsList = new PhysListEmStandard(name);
+
+  } else if (name == "muNucl") {
+
+    fMuNuclPhysicsList = new MuNuclearBuilder(name);*/
+
+  if (name == "FTFP_BERT_EMZ") {
+
+    //physicsList = new FTFP_BERT;
+    G4PhysListFactory factory;
+    physicsList = factory.GetReferencePhysList("FTFP_BERT_EMZ");
+    //G4RunManager* runManager->SetUserInitialization(physList);
+
+  } else {
+
+    G4cout << "PhysicsList::AddPhysicsList: <" << name << ">"
+           << " is not defined"
+           << G4endl;
+  }
+
+  // -- Augment physics list with biasing facilities:
+  G4GenericBiasingPhysics* biasingPhysics = new G4GenericBiasingPhysics();
+  std::vector< G4String > processToBias;
+  processToBias.push_back("muPairProd");
+  biasingPhysics->Bias("mu+", processToBias);
+  physicsList->RegisterPhysics(biasingPhysics);
+  //fEmPhysicsList->RegisterPhysics(biasingPhysics);
+
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+
